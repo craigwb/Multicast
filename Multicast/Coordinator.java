808eb65b86aecef570bcd3beee09c58e0ab7f217
Craig Butler <craigwb@gmx.com>
2018-04-02 22:33:55 -0400

    Add files via upload

--- Multicast/Coordinator.java
+++ Multicast/Coordinator.java
@@ -1,124 +1,138 @@
-
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
 
-
-
-
 public class Coordinator extends Thread {
-    protected Socket socket;
-    public Socket multicast;
-    private CoordMan man;
-    public boolean connected = false;//for multicast
-    public boolean send_message = false;
-    private int last_message_recieved;
-    public Coordinator(Socket socket, CoordMan man) {
-        this.socket = socket;
-        this.man = man;
-        System.out.println("New client connected from " + socket.getInetAddress().getHostAddress());
-        start();
-    }
-    public void multicast(String s) {
-    	try {
-			multicast.getOutputStream().write((s+"\n").getBytes());
+	protected Socket socket;
+	public Socket multicast;
+	private CoordMan man;
+	public boolean connected = false;// for multicast
+	public boolean send_message = false;
+	private int last_message_recieved;
+
+	public Coordinator(Socket socket, CoordMan man) {
+		this.socket = socket;
+		this.man = man;
+		System.out.println("New client connected from " + socket.getInetAddress().getHostAddress());
+		start();
+	}
+
+	public void multicast(String s) {
+		try {
+			multicast.getOutputStream().write((s + "\n").getBytes());
 			multicast.getOutputStream().flush();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    }
-    public void run() {
-        InputStream in = null;
-        OutputStream out = null;
-        try {
-            in = socket.getInputStream();
-            out = socket.getOutputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(in));
-            String request;
-            while ((request = br.readLine()) != null) {
-                System.out.println("Message received:" + request);
-                Scanner args = new Scanner(request);
-                String arg1 = args.next();
-                if(arg1.equalsIgnoreCase("register")) {
-                	send_message=true;
-                	connected = true;
-                	out.write("server: registered\n".getBytes());
-                	multicast = new Socket(socket.getInetAddress(),args.nextInt());
-                }else if(arg1.equalsIgnoreCase("deregister")) {
-                	connected = false;
-                	send_message=false;
-                	multicast = null;
-                	out.write("server: deregistered\n".getBytes());
-                }else if(arg1.equalsIgnoreCase("reconnect")) {
-                	send_message=true;
-                	ServerSocket ss = new ServerSocket(args.nextInt());
-                	multicast = ss.accept();
-                	out.write("server: reconnected\n".getBytes());
-                }else if(arg1.equalsIgnoreCase("deconnect")) {
-                	send_message=false;
-                	multicast = null;
-                	out.write("server: deconnected\n".getBytes());
-                }else if(arg1.equalsIgnoreCase("msend")) {
-                	String m = args.next();
-                	man.multicast(m);
-                	out.write("server: mutlicasted message\n".getBytes());
-                }else out.write("command was not understood\n".getBytes());
-            }
+	}
 
-        } catch (IOException ex) {
-            System.out.println("Unable to get streams from client");
-        } finally {
-            try {
-                in.close();
-                out.close();
-                socket.close();
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        }
-    }
+	public void run() {
+		InputStream in = null;
+		OutputStream out = null;
+		try {
+			in = socket.getInputStream();
+			out = socket.getOutputStream();
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			String request;
+			while ((request = br.readLine()) != null) {
+				System.out.println("Message received:" + request);
+				Scanner args = new Scanner(request);
+				String arg1 = args.next();
+				if (arg1.equalsIgnoreCase("register")) {
+					if (args.hasNextInt()) {
+						send_message = true;
+						connected = true;
+						multicast = new Socket(socket.getInetAddress(), args.nextInt());
+						out.write("server: registered\n".getBytes());
+						out.flush();
+					} else {
+						out.write("server: bad port\n".getBytes());
+						out.flush();
+					}
+				} else if (arg1.equalsIgnoreCase("deregister")) {
+					connected = false;
+					send_message = false;
+					multicast = null;
+					out.write("server: deregistered\n".getBytes());
+					out.flush();
+				} else if (arg1.equalsIgnoreCase("reconnect")) {
+					if (args.hasNextInt()) {
+						send_message = true;
+						connected = true;
+						multicast = new Socket(socket.getInetAddress(), args.nextInt());
+						out.write("server: registered\n".getBytes());
+						out.flush();
+					} else {
+						out.write("server: bad port\n".getBytes());
+						out.flush();
+					}
+				} else if (arg1.equalsIgnoreCase("disconnect")) {
+					send_message = false;
+					multicast = null;
+					out.write("server: disconnected\n".getBytes());
+					out.flush();
+				} else if (arg1.equalsIgnoreCase("msend")) {
+					if(send_message) {
+					String m = args.nextLine();
+					man.multicast(m);
+					out.write("server: mutlicasted message\n".getBytes());
+					out.flush();
+					}else {
+						out.write("server: Please join the multicast group by registering\n".getBytes());
+						out.flush();
+					}
+				} else
+					out.write("command was not understood\n".getBytes());
+				out.flush();
+			}
 
-    public static void main(String[] args) {
-        System.out.println("SocketServer Example");
-      //initial config file
-    	boolean quit = false;
-		int timeThresh=0;
-		int port=0;
+		} catch (IOException ex) {
+			System.out.println("Unable to get streams from client");
+		} finally {
+			try {
+				in.close();
+				out.close();
+				socket.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("SocketServer Example");
+		// initial config file
+		boolean quit = false;
+		int timeThresh = 0;
+		int port = 0;
 		BufferedReader configBufferedReader;
 		String configLine;
-		try { 
-	    configBufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(args[0])));
-	    configLine = configBufferedReader.readLine();
-	    port = Integer.parseInt(configLine);
-	    configLine = configBufferedReader.readLine();
-	    timeThresh = Integer.parseInt(configLine);
-		}
-		catch (Exception e) {
+		try {
+			configBufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(args[0])));
+			configLine = configBufferedReader.readLine();
+			port = Integer.parseInt(configLine);
+			configLine = configBufferedReader.readLine();
+			timeThresh = Integer.parseInt(configLine);
+		} catch (Exception e) {
 			System.out.println(e);
 		}
-		//end initial config 
-        ServerSocket server = null;
-        CoordMan manny = new CoordMan();
-        try {
-            server = new ServerSocket(port);
-            while (true) {
-                /**
-                 * create a new {@link SocketServer} object for each connection
-                 * this will allow multiple client connections
-                 */
-            	manny.add_coord(server.accept(), manny);
-                
-            }
-        } catch (IOException ex) {
-            System.out.println("Unable to start server.");
-        } finally {
-            try {
-                if (server != null)
-                    server.close();
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        }
-    }
+		// end initial config
+		ServerSocket server = null;
+		CoordMan manny = new CoordMan();
+		try {
+			server = new ServerSocket(port);
+			while (true) {
+				manny.add_coord(server.accept(), manny);
+			}
+		} catch (IOException ex) {
+			System.out.println("Unable to start server.");
+		} finally {
+			try {
+				if (server != null)
+					server.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
 }
--- Multicast/Participant.java
+++ Multicast/Participant.java
@@ -7,141 +7,144 @@ import java.util.concurrent.TimeUnit;
 
 public class Participant {
 	private mult_listener mult;
-	private Socket echoSocket;
+	private Socket socket;
 	private PrintWriter out;
 	private BufferedReader in;
 	private BufferedReader stdIn;
 	private BufferedWriter bw;
+	private boolean quit = false;
 	public static void main(String args[]) {
 		// initial config
-				boolean exit = false;
-				String ID = "";
-				int port = 0;
-				BufferedReader configBufferedReader;
-				BufferedWriter bw = null;
-				String configLine;
-				String host = "";
-				FileWriter fw=null;
-				try {
-					configBufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(args[0])));
-					configLine = configBufferedReader.readLine();
-					ID = configLine;
-					configLine = configBufferedReader.readLine();
-					//fw = new FileWriter(configLine, true);
-					//bw = new BufferedWriter(fw);
-					//PrintWriter printWriter = new PrintWriter(fw);
-					String s = "initialized\n";
-					//printWriter.write(s);
-					//FileOutputStream out = new FileOutputStream(configLine);
-					 bw  = new BufferedWriter(new FileWriter(configLine, true));
-					 bw.append(s);
-					 bw.flush();
-					System.out.println(configLine);
-					configLine = configBufferedReader.readLine();
-					Scanner scan = new Scanner(configLine);
-					host = scan.next();
-					port = scan.nextInt();
-					scan.close();
-				} catch (Exception e) {
-					System.out.println(e);
-				}
-				// end initial config
-        new Participant(host, port, ID, bw);
-    }
+		String ID = "";
+		int port = 0;
+		BufferedReader configBufferedReader;
+		BufferedWriter bw = null;
+		String configLine;
+		String host = "";
+		try {
+			configBufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(args[0])));
+			configLine = configBufferedReader.readLine();
+			ID = configLine;
+			configLine = configBufferedReader.readLine();
+			String s = "System Time: " + System.currentTimeMillis() + " Participant Started\n";
+			bw = new BufferedWriter(new FileWriter(configLine, true));
+			bw.append(s);
+			bw.flush();
+			System.out.println(configLine);
+			configLine = configBufferedReader.readLine();
+			Scanner scan = new Scanner(configLine);
+			host = scan.next();
+			port = scan.nextInt();
+			scan.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		// end initial config
+		new Participant(host, port, ID, bw);
+	}
 
-    public Participant(String host, int port, String ID, BufferedWriter bw) {
-        try {
-        	this.bw = bw;
-            System.out.println("Connecting to host " + host + " on port " + port + ".");
-            try {
-                echoSocket = new Socket(host, 8081);
-                out = new PrintWriter(echoSocket.getOutputStream(), true);
-                in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
-            } catch (UnknownHostException e) {
-                System.err.println("Unknown host: " + host );
-                System.exit(1);
-            } catch (IOException e) {
-                System.err.println("Unable to get streams from server");
-                System.exit(1);
-            }
-
-            /** {@link UnknownHost} object used to read from console */
-            stdIn = new BufferedReader(new InputStreamReader(System.in));
-
-            while (true) {
-                System.out.print(ID+": ");
-                String userInput = stdIn.readLine();
-                Scanner args = new Scanner(userInput);
-                String arg1 = args.next();
-                out.println(userInput);
-                if (userInput.equals("quit")) {
-                    break;
-                }else if (arg1.equals("register")) {
-                	//ServerSocket multi_accept = new ServerSocket(args.nextInt());
-                	try {
-                		ServerSocket ss = new ServerSocket(args.nextInt());
-                		TimeUnit.SECONDS.sleep(5);
-                		mult = new mult_listener(ss.accept());
-                		bw.append("listening\n");
-                		bw.flush();
-                	} catch (Exception e) {
-                		e.printStackTrace();
-                	}
-                }else if (arg1.equals("deregister")) {
-                	mult = null;
-                }else if (arg1.equals("reconnect")) {
-                	try {
-                		mult = new mult_listener(new Socket(host,args.nextInt()));
-                	} catch (Exception e) {
-                		e.printStackTrace();
-                	}
-                }else if (arg1.equals("deconnect")) {
-                	mult=null;
-                }else if (arg1.equals("msend")) {
-                }
-                String server_says = in.readLine();
-                
-                System.out.println(server_says);
-                
-                
-            }
-
-            /** Closing all the resources */
-            out.close();
-            in.close();
-            stdIn.close();
-            echoSocket.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    class mult_listener extends Thread implements Runnable{
-    	Socket multic;
-    	BufferedReader in;
-    	mult_listener(Socket socket){
-    		this.multic = socket;
-    		try{
-    			in = new BufferedReader(new InputStreamReader(multic.getInputStream()));
-    		}catch (Exception e) {
-    			e.printStackTrace();
-    		}
-    		System.out.println("Starting listening for multicast");
-    		start();
-    	}
-    	public void run() {
-    		String message;
+	public Participant(String host, int port, String ID, BufferedWriter bw) {
+		try {
+			this.bw = bw;
+			System.out.println("Connecting to host " + host + " on port " + port + ".");
 			try {
-	            //while ((message = in.readLine()) != null) {
-	            while ((message = in.readLine())!=null) {
-	            	//System.out.println(message);
-	            	bw.append(message+"\n");
-	            	bw.flush();
-	    		}
+				socket = new Socket(host, 8081);
+				out = new PrintWriter(socket.getOutputStream(), true);
+				in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-    		
-    	}
-    	
-    }
+
+			/** {@link UnknownHost} object used to read from console */
+			stdIn = new BufferedReader(new InputStreamReader(System.in));
+			ServerSocket ss=null;
+			while (!quit) {
+				System.out.print(ID + ": ");
+				String userInput = stdIn.readLine();
+				Scanner args = new Scanner(userInput);
+				String arg1 = args.next();
+				out.println(userInput);
+				if (userInput.equals("quit")) {
+					quit=true;
+				} else if (arg1.equals("register")) {
+					// ServerSocket multi_accept = new ServerSocket(args.nextInt());
+					try {
+						if (args.hasNextInt()) {
+						ss = new ServerSocket(args.nextInt());
+						System.out.println("Allow system 5 seconds to connect.");
+						TimeUnit.SECONDS.sleep(5);
+						mult = new mult_listener(ss.accept());
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				} else if (arg1.equals("deregister")) {
+					ss.close();
+					mult = null;
+					TimeUnit.SECONDS.sleep(2);
+				} else if (arg1.equals("reconnect")) {
+					try {
+						if (args.hasNextInt()) {
+							ss = new ServerSocket(args.nextInt());
+							System.out.println("Allow system 5 seconds to connect.");
+							TimeUnit.SECONDS.sleep(5);
+							mult = new mult_listener(ss.accept());
+							}
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				} else if (arg1.equals("disconnect")) {
+					ss.close();
+					mult = null;
+					TimeUnit.SECONDS.sleep(2);
+				} else if (arg1.equals("msend")) {
+				}
+				String server_says = in.readLine();
+
+				System.out.println(server_says);
+
+			}
+
+			/** Closing all the resources */
+			out.close();
+			in.close();
+			stdIn.close();
+			socket.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	class mult_listener extends Thread implements Runnable {
+		Socket multic;
+		BufferedReader in;
+
+		mult_listener(Socket socket) {
+			this.multic = socket;
+			try {
+				in = new BufferedReader(new InputStreamReader(multic.getInputStream()));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			System.out.println("Started listening for multicast");
+			start();
+		}
+
+		public void run() {
+			String message;
+			try {
+				// while ((message = in.readLine()) != null) {
+				while ((message = in.readLine()) != null) {
+					// System.out.println(message);
+					bw.append("System Time: " + System.currentTimeMillis() + " Message Number In Session: " + message
+							+ "\n");
+					bw.flush();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		}
+
+	}
 }
